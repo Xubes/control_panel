b3a4160e90c77feb00c2bdfe0e59e8b35f2b8ba1
Shohan <shohan.hasan.x6@gmail.com>
2013-05-11 11:58:06 -0400

    analog mode

--- control_panel.ino
+++ control_panel.ino
@@ -4,9 +4,10 @@ const int rgbPins[] = {9,10,11};
 #define ON LOW
 #define OFF HIGH
 
-boolean RED[] = {ON, OFF, OFF};
-boolean GREEN[] = {OFF, ON, OFF};
-boolean BLUE[] = {OFF, OFF, ON};
+double RED[] = {0, 1, 1};
+double GREEN[] = {1, 0, 1};
+double BLUE[] = {1, 1, 0};
+double TEST[] = {1, 1, .8};
 
 void setup(){
   Serial.begin(9600);
@@ -14,20 +15,34 @@ void setup(){
   for(int i=0; i<3; i++){
     pinMode(rgbPins[i],OUTPUT);
   }
-  
+   
 }
 
 void loop(){
-  setColor(RED);
+  setColor(TEST);
   delay(1000);
   setColor(GREEN);
   delay(100);
-  setColor(BLUE);
-  delay(5000);
 }
 
-void setColor(boolean color[]){
+void setColor(double color[]){
   for(int i=0; i<3; i++){
-    digitalWrite(rgbPins[i],color[i]);
+    analogWrite(rgbPins[i],255*color[i]);
   }
+}
+
+//cycle through all colors
+void test(){
+  double ss = 0.1;
+  for(double r=1.0; r>0; r-=ss){
+    for(double g=1.0; g>0; g-=ss){
+      for(double b=1.0; b>0; b-=ss){
+        TEST[0] = r;
+        TEST[1] = g;
+        TEST[2] = b;
+        setColor(TEST);
+        delay(50);
+      }
+    }
+  } 
 }
